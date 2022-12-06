5bf0ed9404f11dd500e2fbd573041899ef6776c4
aumarin <aumarin@student.42.fr>
2022-12-07 00:56:03 +0100

    fix: function name:

--- ft_max.c
+++ ft_max.c
@@ -6,11 +6,11 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 08:50:40 by aumarin           #+#    #+#             */
-/*   Updated: 2022/12/06 08:51:09 by aumarin          ###   ########.fr       */
+/*   Updated: 2022/12/07 00:55:04 by aumarin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	max(int a, int b)
+int	ft_max(int a, int b)
 {
 	if (a > b)
 		return (a);
